5e572d6c3736f985ffa242f3876d774a918ac760
Agustin Carril <carrilagustin@gmail.com>
2021-10-26 22:15:40 -0300

    Add navbar - Agustin Carril

---
+++ src/components/NavBar/navbar.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+function navbar() {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">MONSTER STORE</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Categories
+                        </a>
+                        <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a className="dropdown-item" href="#">Action</a></li>
+                            <li><a className="dropdown-item" href="#">Another action</a></li>
+                            <li><a className="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link" href="#">Products</a>
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default navbar
